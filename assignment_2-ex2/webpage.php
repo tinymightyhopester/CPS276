d70c4e3445c467db786ef90bea429770599ddfd3
Hope <creatinghopeagain@gmail.com>
2023-02-11 22:58:25 -0500

    copyright

--- assignment_2-ex2/webpage.php
+++ assignment_2-ex2/webpage.php
@@ -4,7 +4,7 @@ $header = "My Goddamn Web Page";
 $name = "Hope Heck";
 $paragraph = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus feugiat mollis dolor at bibendum. In congue maximus ligula, ut faucibus mi accumsan at. Vestibulum sagittis tortor eget dui ultricies, a vulputate lacus faucibus. Fusce aliquet bibendum erat, sed bibendum eros cursus eu. Nulla at neque rhoncus, ultricies odio at, accumsan elit. Proin in turpis eu leo dapibus pulvinar. Vivamus viverra massa ut enim fringilla ultricies. Donec in enim blandit, iaculis nulla quis, egestas elit. Nullam ut enim id erat bibendum finibus nec ac eros. Nulla malesuada ex facilisis ultrices rhoncus. Nullam in euismod nisl. Donec pulvinar ex sit amet aliquet egestas. <br>";
 $string = "";
-$footer = "and here's your damn php footer";
+$footer = "and here's your damn php footer ©2023";
 
 for ($i=1; $i<=3; $i++){
 	$string = $string.$paragraph."<br>";
