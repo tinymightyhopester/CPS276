12b545bc09462778091199c7253497254db0610b
Hope <creatinghopeagain@gmail.com>
2023-02-12 18:43:47 -0500

    looptest

--- assignment_2-ex1/webpage.php
+++ assignment_2-ex1/webpage.php
@@ -3,13 +3,16 @@
 $string = "";
 
 
-for ($i=1; $i<=3; $i++){
-	$string = $string."<br>";
-   }
+for ($i = 1; $i < 5; $i++){
+	$string .= $i . "<br>";
+	for ($j = 1; $j < 4; $j++){
+		$string .= "&nbsp;&nbsp;&nbsp;&nbsp;".$j."<br>";
+	}
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <body>
-	
+	<p><?php echo $string; ?></p>
 </body>
 </html>
