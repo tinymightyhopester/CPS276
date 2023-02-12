9c9cae0fae5ead6d132ab1cb559323f249481f9b
Hope <creatinghopeagain@gmail.com>
2023-02-12 14:55:53 -0500

    ex3

--- assignment_2-ex3/webpage.php
+++ assignment_2-ex3/webpage.php
@@ -1,5 +1,16 @@
 <?php
+   $i = 0;
+   $j = 0;
 
+   $table = "<table border = '1'>";
+
+   while($i < 5){
+      $table .= "<tr>";
+      $j = 0;
+      while($j < 3){
+         $table .= "<td>cell</td>";
+      }
+   }
 ?>
 <!DOCTYPE html>
 <html lang="en">
