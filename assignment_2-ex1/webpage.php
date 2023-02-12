31b29add1ed3b8dbd2faadd9cce5cf82583ec305
Hope <creatinghopeagain@gmail.com>
2023-02-12 18:52:31 -0500

    ul

--- assignment_2-ex1/webpage.php
+++ assignment_2-ex1/webpage.php
@@ -1,14 +1,18 @@
 <?php
 
-$string = "";
-
+$string = "<ul>";
 
 for ($i = 1; $i < 5; $i++){
-	$string .= $i . "<br>";
+	$string .= "<li>".$i."</li>";
+	$string .= "<ul>";
 	for ($j = 1; $j < 4; $j++){
-		$string .= "&nbsp;&nbsp;&nbsp;&nbsp;".$j."<br>";
+		$string .= "&nbsp;&nbsp;&nbsp;&nbsp; <li>".$j."</li>";
 	}
+	$string .= "</ul>";
 }
+
+$string .= "</ul>";
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
