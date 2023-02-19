a15ace58488d570caaa5d9fb6418d7c9cdc4cf6e
Hope <creatinghopeagain@gmail.com>
2023-02-19 15:47:25 -0500

    num2

--- assignment_3/Calculator.php
+++ assignment_3/Calculator.php
@@ -3,10 +3,10 @@ class Calculator{
     
     public function calc($operator=null, $num1=null, $num2=null){
         
-        if (is_null($operator) or is_null($num1) or is_null($num1)){
+        if (is_null($operator) or is_null($num1) or is_null($num2)){
             return "Must enter a string and two numbers";
         }
-        else if (is_string($operator) && is_int($num1) && is_int($num1))
+        else if (is_string($operator) && is_int($num1) && is_int($num2))
         {
             if ($num2 == 0){
                 return "Cannot divide by zero".". \n";
