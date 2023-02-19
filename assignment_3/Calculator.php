af2d67131c22edb55db92d9b46e07843160e8694
Hope <creatinghopeagain@gmail.com>
2023-02-19 15:36:03 -0500

    catch

--- assignment_3/Calculator.php
+++ assignment_3/Calculator.php
@@ -3,39 +3,40 @@ class Calculator{
     
     public function calc($operator, $num1, $num2){
         
-        if (is_null($operator) or is_null($num1) or is_null($num1)){
-            return "Must enter a string and two numbers";
-        }
-        else if (is_string($operator) && is_int($num1) && is_int($num1))
-        {
-            if ($num2 == 0){
-                return "Cannot divide by zero";
-            }
-            else{
-                if($operator == "/"){
+        try{
+            if (is_string($operator) && is_int($num1) && is_int($num1))
+            {
+                if ($num2 == 0){
+                    return "Cannot divide by zero";
+                }
+                else{
+                    if($operator == "/"){
 
-                    $x = $num1/$num2;
-                    return "The division of the numbers is ".$x."\n";
+                        $x = $num1/$num2;
+                        return "The division of the numbers is ".$x."\n";
 
-                }else if($operator == "*"){
+                    }else if($operator == "*"){
 
-                    $x = $num1*$num2;
-                    return "The product of the numbers is ".$x."\n";
+                        $x = $num1*$num2;
+                        return "The product of the numbers is ".$x."\n";
 
-                }else if($operator == "-"){
+                    }else if($operator == "-"){
 
-                    $x = $num1-$num2;
-                    return "The difference of the numbers is ".$x."\n";
+                        $x = $num1-$num2;
+                        return "The difference of the numbers is ".$x."\n";
 
-                }else if($operator == "+"){
+                    }else if($operator == "+"){
 
-                    $x = $num1+$num2;
-                    return "The sum of the numbers is ".$x."\n";
+                        $x = $num1+$num2;
+                        return "The sum of the numbers is ".$x."\n";
 
-                }else{
-                    return "invalid operator";
+                    }else{
+                        return "invalid operator";
+                    }
                 }
             }
+        }catch (Exception $e){
+            return "Must enter a string and two numbers";
         }
     }
 
