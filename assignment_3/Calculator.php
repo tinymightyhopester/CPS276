6f47924c03978dafbeda5935140a116c305ca657
Hope <creatinghopeagain@gmail.com>
2023-02-19 15:44:59 -0500

    format

--- assignment_3/Calculator.php
+++ assignment_3/Calculator.php
@@ -9,28 +9,28 @@ class Calculator{
         else if (is_string($operator) && is_int($num1) && is_int($num1))
         {
             if ($num2 == 0){
-                return "Cannot divide by zero";
+                return "Cannot divide by zero".". \n";
             }
             else{
                 if($operator == "/"){
 
                     $x = $num1/$num2;
-                    return "The division of the numbers is ".$x."\n";
+                    return "The division of the numbers is ".$x.". \n";
 
                 }else if($operator == "*"){
 
                     $x = $num1*$num2;
-                    return "The product of the numbers is ".$x."\n";
+                    return "The product of the numbers is ".$x.". \n";
 
                 }else if($operator == "-"){
 
                     $x = $num1-$num2;
-                    return "The difference of the numbers is ".$x."\n";
+                    return "The difference of the numbers is ".$x.". \n";
 
                 }else if($operator == "+"){
 
                     $x = $num1+$num2;
-                    return "The sum of the numbers is ".$x."\n";
+                    return "The sum of the numbers is ".$x.". \n";
 
                 }else{
                     return "invalid operator";
