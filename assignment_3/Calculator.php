a4e6e1a8383e89c828bfdc99a0a5ed746e6c318a
Hope <creatinghopeagain@gmail.com>
2023-02-19 14:51:40 -0500

    sc

--- assignment_3/Calculator.php
+++ assignment_3/Calculator.php
@@ -6,8 +6,8 @@ class Calculator{
         if ($operator.is_string && $num1.is_int && $num2.is_int)
         {
             if ($num2 == 0){
-                return "Cannot divide by zero"
-            };
+                return "Cannot divide by zero";
+            }
             else{
                 if($operator == "/"){
 
@@ -32,11 +32,11 @@ class Calculator{
                 }else{
                     return "invalid operator";
                 }
-            };
+            }
         }
         else{
             return "Must enter a string and two numbers";
-        };
+        }
     }
 
 }
