300c67a132dc6407dab070e288aa04ea12e2384d
Hope <creatinghopeagain@gmail.com>
2023-02-19 15:04:33 -0500

    nvm

--- assignment_3/customer_page.php
+++ assignment_3/customer_page.php
@@ -6,7 +6,7 @@ $Calculator = new Calculator();
 <html lang="en">
 <body>
 	<p><?php
-echo $Calculator->calc("/", 10, 0)<br>; //will output Cannot divide by zero
+echo $Calculator->calc("/", 10, 0); //will output Cannot divide by zero
 echo $Calculator->calc("*", 10, 2); //will output The product of the numbers is 20
 echo $Calculator->calc("/", 10, 2); //will output The division of the numbers is 5
 echo $Calculator->calc("-", 10, 2); //will output The difference of the numbers is 8
