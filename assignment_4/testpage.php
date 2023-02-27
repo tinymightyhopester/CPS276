bc145a6b32ab1f02c4bae60b1a995d6976d93aa8
Hope <creatinghopeagain@gmail.com>
2023-02-26 20:09:39 -0500

    update4

--- assignment_4/testpage.php
+++ assignment_4/testpage.php
@@ -6,8 +6,8 @@
     <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
   </div>
   <div class="mb-3">
-    <label for="exampleInputPassword1" class="form-label">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1">
+    <label for="inputName" class="form-label">Name</label>
+    <div><input type="Name" class="form-control" id="inputName"></div>
   </div>
   
   <button type="submit" class="btn btn-primary">Submit</button>
