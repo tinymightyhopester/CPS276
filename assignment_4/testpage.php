f76648996bc91544c09e575422dfac0493d2699b
Hope <creatinghopeagain@gmail.com>
2023-02-26 22:07:19 -0500

    update4

--- assignment_4/testpage.php
+++ assignment_4/testpage.php
@@ -6,9 +6,10 @@
   <button type="submit" class="btn btn-primary">Add Name</button>
   <button type="submit" class="btn btn-secondary">Clear</button>
   <div class="mb-3">
-    <label for="inputName" class="form-label">Name</label>
+    <label for="inputName" class="form-label">Enter Name</label>
     <div><input type="Name" class="form-control" id="inputName" aria-describedby="input name"></div>
   </div>
-  
-  
+  <div>
+  <textarea style="height: 500px;" class="form-control" id="namelist" name="namelist"><?php  ?></textarea>
+  </div>
 </form>
