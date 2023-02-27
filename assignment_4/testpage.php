a84fca23b8335d417c73e42407af523cd0fff2df
Hope <creatinghopeagain@gmail.com>
2023-02-26 20:12:02 -0500

    4

--- assignment_4/testpage.php
+++ assignment_4/testpage.php
@@ -5,10 +5,11 @@
     <label for="exampleInputEmail1" class="form-label">Email address</label>
     <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
   </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
   <div class="mb-3">
     <label for="inputName" class="form-label">Name</label>
-    <div><input type="Name" class="form-control" id="inputName"></div>
+    <div><input type="Name" class="form-control" id="inputName" aria-describedby="input name"></div>
   </div>
   
-  <button type="submit" class="btn btn-primary">Submit</button>
+  
 </form>
