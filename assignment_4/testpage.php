b2966fc6ad10fdd70a1a94d7b637ba00d7c0080d
Hope <creatinghopeagain@gmail.com>
2023-02-26 21:02:39 -0500

    page update

--- assignment_4/testpage.php
+++ assignment_4/testpage.php
@@ -2,11 +2,9 @@
 ?>
 <form>
   <h1>Add Names</h1>
-  <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-  </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
+  
+  <button type="submit" class="btn btn-primary">Add Name</button>
+  <button type="submit" class="btn btn-secondary">Clear</button>
   <div class="mb-3">
     <label for="inputName" class="form-label">Name</label>
     <div><input type="Name" class="form-control" id="inputName" aria-describedby="input name"></div>
