848181c76f7f7778efb0c9219f430297daa7e3b7
Hope <creatinghopeagain@gmail.com>
2023-03-12 13:26:12 -0400

    ud

--- assignment_5/directoryPage.php
+++ assignment_5/directoryPage.php
@@ -42,7 +42,7 @@ if(isset($_POST['create'])){
     </form>
         <div>
             <textarea style="height: 500px;" class="form-control" id="text" name="text">
-            <?php  echo  ?></textarea>
+            <?php  ?></textarea>
         </div>
     </div>
   </body>
