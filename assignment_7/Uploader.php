af75ce138f2159abc44e68eded6d186d6bdec2ed
Hope <creatinghopeagain@gmail.com>
2023-03-26 01:33:39 -0400

    specifications

--- assignment_7/Uploader.php
+++ assignment_7/Uploader.php
@@ -16,15 +16,14 @@ if(isset($_POST["submit"])) {
   }
 }
 // Check file size
-if ($_FILES["fileToUpload"]["size"] > 500000) {
+if ($_FILES["fileToUpload"]["size"] > 100000) {
     echo "Sorry, your file is too large.";
     $uploadOk = 0;
   }
   
 // Allow certain file formats
-if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-&& $imageFileType != "gif" ) {
-  echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+if($imageFileType != "pdf") {
+  echo "Sorry, only PDF files are allowed.";
   $uploadOk = 0;
 }
 
