14e13fecd743837000ea45b1c9a48d4db5ee43af
Hope <creatinghopeagain@gmail.com>
2023-03-27 12:41:23 -0400

    upload boot test

--- assignment_7/uploadForm.php
+++ assignment_7/uploadForm.php
@@ -1,7 +1,11 @@
 
 <!DOCTYPE html>
+<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
 <html>
-<body>
+<body style="padding: 30px;">
 
 <form action="Uploader.php" method="post" enctype="multipart/form-data">
     <h1>File upload</h1>
