9b99cfd1b1f0d81569a5d20d9b77466098e778a6
Hope <creatinghopeagain@gmail.com>
2023-03-26 22:42:24 -0400

    format

--- assignment_7/uploadForm.php
+++ assignment_7/uploadForm.php
@@ -5,6 +5,7 @@
 
 <form action="Uploader.php" method="post" enctype="multipart/form-data">
   Select image to upload:
+  <input type="text" name="fileName" id="fileName">
   <input type="file" name="fileToUpload" id="fileToUpload">
   <input type="submit" value="Upload File" name="submit">
 </form>
