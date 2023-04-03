6a3ce4b95b1ed8f14c9a721c23781d6551e45f89
Hope <creatinghopeagain@gmail.com>
2023-04-02 23:23:24 -0400

    syntax

--- assignment_8/php/clearNames.php
+++ assignment_8/php/clearNames.php
@@ -3,7 +3,7 @@
 require_once "/home/h/h/hheck/public_html/CPS276/assignment_8/classes/CRUD.php";
 $crud = new CRUD();
 
-$records = $crud->clearNames;
+$records = $crud->clearNames();
 
 $response = (object)[
     'masterstatus'=>'success',
