93efd3de2e7111d446b2fe8c943256329e35ce02
Hope <creatinghopeagain@gmail.com>
2023-04-07 17:03:13 -0400

    html setup

---
+++ assignment_9/add.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Add Note</title>
+
+    
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    
+  </head>
+  <body>
+    <div class="container">
+      <h1>Add Note</h1>
+
+      <p>Display notes href</p>
+
+      <form method="post" action="">
+        <div class="form-group">
+            <label for="dateTime">Date and Time</label>
+            <input type="text" class="form-control" id="dateTime" name="dateTime">
+        </div>
+        <div class="form-group">
+            <label for="note">File Content</label>
+            <textarea name="note" id="filecontent" name="note" class="form-control" cols="30" rows="20"></textarea>
+        </div>
+        <button type="submit" class="btn btn-primary" name="submit">Submit</button>
+      </form>
+    </div>
+
+  </body>
+</html>
---
+++ assignment_9/display.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Notes</title>
+
+    
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    
+  </head>
+  <body>
+    <div class="container">
+      <h1>Display Notes</h1>
+
+      <p>add note href</p>
+
+      <form method="post" action="">
+        <div class="form-group">
+            <label for="beginningDate">Beginning Date</label>
+            <input type="text" class="form-control" id="beginningDate" name="beginningDate">
+        </div>
+        <div class="form-group">
+            <label for="endDate">End Date</label>
+            <input type="text" class="form-control" id="endDate" name="endDate">
+        </div>
+        <button type="submit" class="btn btn-primary" name="submit">Submit</button>
+      </form>
+    </div>
+
+  </body>
+</html>
