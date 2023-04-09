5ad530525e92174965ee660265cd5d4644d6f8e7
Hope <creatinghopeagain@gmail.com>
2023-04-09 19:14:49 -0400

    update

--- assignment_9/add.php
+++ assignment_9/add.php
@@ -1,7 +1,12 @@
 <?php
-//require_once '/home/h/h/hheck/public_html/CPS276/assignment_9/classes/date_note.php';
-//$dateNote = new date_note();
 
+if(count($_POST)){
+  require_once 'classes/date_note.php';
+  $dateNote = new date_note();
+  $msg = newDateNote();
+}else{
+  $msg = "";
+}
 ?>
 <!doctype html>
 
@@ -21,7 +26,7 @@
     <div class="container">
       <h1>Add Note</h1>
 
-      <?php //echo $dateNote->newDateNote(); ?>
+      <?php echo $msg ?>
 
       <p><a href='https://russet-v8.wccnet.edu/~hheck/CPS276/assignment_9/display.php'>Display Notes</a></p>
 
