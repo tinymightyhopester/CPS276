9fcd597148a52628ccb0b8e91c1bf9f261cc2d4c
Hope <creatinghopeagain@gmail.com>
2023-04-09 15:33:23 -0400

    ()

--- assignment_9/add.php
+++ assignment_9/add.php
@@ -1,7 +1,7 @@
 <?php
 require_once '/home/h/h/hheck/public_html/CPS276/assignment_9/classes/date_note.php';
 $dateNote = new date_note();
-$msg = $dateNote->newDateNote($_POST);
+$msg = $dateNote->newDateNote();
 ?>
 <!doctype html>
 
