f8968b3c47daadd01a92164e1a201650731c47a3
Hope <creatinghopeagain@gmail.com>
2023-04-09 19:23:17 -0400

    updates

---
+++ assignment_9_1/add.php
@@ -0,0 +1,42 @@
+<?php
+require_once '/home/h/h/hheck/public_html/CPS276/assignment_9_1/classes/date_note.php';
+$dateNote = new date_note();
+
+?>
+<!doctype html>
+
+<html lang="en">
+  <head>
+    
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Add Note</title>
+
+    
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    
+  </head>
+  <body>
+    <div class="container">
+      <h1>Add Note</h1>
+
+      <?php echo $dateNote->newDateNote(); ?>
+
+      <p><a href='https://russet-v8.wccnet.edu/~hheck/CPS276/assignment_9/display.php'>Display Notes</a></p>
+
+      <form method="post" action="add.php">
+        <div class="form-group">
+            <label for="dateTime">Date and Time</label>
+            <input type="datetime-local" class="form-control" id="dataTime" name="dateTime">
+        </div>
+        <div class="form-group">
+            <label for="note">File Content</label>
+            <textarea name="note" id="filecontent" name="note" class="form-control" cols="30" rows="20"></textarea>
+        </div>
+        <button type="submit" class="btn btn-primary" name="addSubmit">Submit</button>
+      </form>
+    </div>
+
+  </body>
+</html>
---
+++ assignment_9_1/display.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Notes</title>
+
+    
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    
+  </head>
+  <body>
+    <div class="container">
+      <h1>Display Notes</h1>
+
+      <p><a href='https://russet-v8.wccnet.edu/~hheck/CPS276/assignment_9/add.php'>Add Note</a></p>
+
+      <form method="post" action="date_note.php">
+        <div class="form-group">
+            <label for="beginningDate">Beginning Date</label>
+            <input type="date" class="form-control" id="beginningDate" name="beginningDate">
+        </div>
+        <div class="form-group">
+            <label for="endDate">End Date</label>
+            <input type="date" class="form-control" id="endDate" name="endDate">
+        </div>
+        <button type="submit" class="btn btn-primary" name="displaySubmit">Submit</button>
+      </form>
+    </div>
+
+  </body>
+</html>
