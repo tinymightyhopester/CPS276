af099a2267893f2c3264551ebd3f31c13ab4269f
Hope <creatinghopeagain@gmail.com>
2023-04-16 12:46:40 -0400

    sql

---
+++ assignment_10/sql/admins.sql
@@ -0,0 +1,9 @@
+########################
+# Create admins table
+########################
+CREATE TABLE admins
+(
+  id            int          NOT NULL AUTO_INCREMENT,
+  
+  PRIMARY KEY (id)
+) ENGINE=InnoDB;
---
+++ assignment_10/sql/contactsTable.sql
@@ -0,0 +1,9 @@
+########################
+# Create contacts table
+########################
+CREATE TABLE contacts
+(
+  id            int          NOT NULL AUTO_INCREMENT,
+  
+  PRIMARY KEY (id)
+) ENGINE=InnoDB;
