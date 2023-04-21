eab378cbd5507268f95c73b7fbdbfe80c7041069
Hope <creatinghopeagain@gmail.com>
2023-04-21 12:28:59 -0400

    testing

--- assignment_10/sql/contactsTable.sql
+++ assignment_10/sql/contactsTable.sql
@@ -3,7 +3,15 @@
 ########################
 CREATE TABLE contacts
 (
-  id            int          NOT NULL AUTO_INCREMENT,
-  
+  id       int          NOT NULL AUTO_INCREMENT,
+  name     varchar(140)  NULL ,
+  address  varchar(45)  NULL ,
+  city     varchar(45)  NULL ,
+  state    varchar(45)  NULL ,
+  phone    varchar(45)  NULL ,
+  email    varchar(45)  NULL ,
+  dob      varchar(45)  NULL ,
+  contacts varchar(140) NULL,
+  age      int NULL,
   PRIMARY KEY (id)
 ) ENGINE=InnoDB;
