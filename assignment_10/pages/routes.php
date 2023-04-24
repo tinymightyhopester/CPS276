411a7442e44ed2d2903f71bc65162befba14c942
Hope <creatinghopeagain@gmail.com>
2023-04-24 13:58:04 -0400

    routes and welcome

---
+++ assignment_10/pages/routes.php
@@ -0,0 +1,56 @@
+<?php
+
+$path = "index.php?page=welcome";
+
+
+$nav=<<<HTML
+    <nav>
+        <ul>
+            <li><a href="index.php?page=welcome">Welcome</a></li>
+            <li><a href="index.php?page=addContact">Add Contact Information</a></li>
+            <li><a href="index.php?page=deleteContacts">Delete Contact(s)</a></li>
+            <li><a href="index.php?page=addAdmin">Add Admin</a></li>
+            <li><a href="index.php?page=deleteAdmins">Delete Admin(s)</a></li>
+        </ul>
+    </nav>
+HTML;
+
+if(isset($_GET)){
+    if($_GET['page'] === "addContact"){
+        require_once('pages/addContact.php');
+        $result = init();
+    }
+    
+    else if($_GET['page'] === "deleteContacts"){
+        require_once('pages/deleteContacts.php');
+        $result = init();
+    }
+
+    if($_GET['page'] === "addAdmin"){
+        require_once('pages/addAdmin.php');
+        $result = init();
+    }
+    
+    else if($_GET['page'] === "deleteAdmins"){
+        require_once('pages/deleteAdmins.php');
+        $result = init();
+    }
+
+    else if($_GET['page'] === "welcome"){
+        require_once('pages/welcome.php');
+        $result = init();
+
+    }
+
+    else {
+        header('location: '.$path);
+    }
+}
+
+else {
+    header('location: '.$path);
+}
+
+
+
+?>
