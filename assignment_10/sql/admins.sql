2daa0b527796adec8ccb7536d90ff1c9077ef8ce
Hope <creatinghopeagain@gmail.com>
2023-04-24 13:50:51 -0400

    format

--- assignment_10/sql/admins.sql
+++ assignment_10/sql/admins.sql
@@ -6,10 +6,10 @@ DROP TABLE IF EXISTS `admins`;
 CREATE TABLE admins
 (
   id            int          NOT NULL AUTO_INCREMENT,
-  name     varchar(140)  NULL ,
-  email    varchar(45)  NULL ,
-  password    varchar(140)  NULL ,
-  status    varchar(45)  NULL ,
+  name      varchar(140)  NULL ,
+  email     varchar(45)   NULL ,
+  password  varchar(140)  NULL ,
+  status    varchar(45)   NULL ,
   PRIMARY KEY (id)
 ) ENGINE=InnoDB;
 
