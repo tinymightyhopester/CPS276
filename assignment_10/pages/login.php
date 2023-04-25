9b1cb5e8861af1eb093ecf7285f0737698b0656d
Hope <creatinghopeagain@gmail.com>
2023-04-25 14:56:16 -0400

    updates

--- assignment_10/pages/login.php
+++ assignment_10/pages/login.php
@@ -2,7 +2,6 @@
 
 require_once('classes/StickyForm.php');
 $stickyForm = new StickyForm();
-$error = '';
 
 function init(){
     global $elementsArr, $stickyForm;
@@ -55,75 +54,26 @@ $elementsArr = [
         "errorMessage"=>"<span style='color: red; margin-left: 15px;'>Password cannot be blank and must be a valid password</span>",
         "errorOutput"=>"",
         "type"=>"text",
-        "value"=>"",
-        "regex"=>"password"
+        "value"=>""
     ]
-  ];
-
-  function addData($post){
-    global $elementsArr;  
-    /* IF EVERYTHING WORKS ADD THE DATA HERE TO THE DATABASE HERE USING THE $_POST SUPER GLOBAL ARRAY */
-        //print_r($_POST);
-        require_once('classes/Pdo_methods.php');
-  
-        $pdo = new Pdo_methods();
-  
-        $sql = "";
-  
-        /* THIS TAKE THE ARRAY OF CHECK BOXES AND PUT THE VALUES INTO A STRING SEPERATED BY COMMAS  */
-        if(isset($_POST['financial'])){
-          $financial = "";
-          foreach($post['financial'] as $v){
-            $financial .= $v.",";
-          }
-          /* REMOVE THE LAST COMMA FROM THE CONTACTS */
-          $financial = substr($financial, 0, -1);
-        }
-  
-        if(isset($_POST['eyeColor'])){
-          $eyeColor = $_POST['eyeColor'];
-        }
-        else {
-          $eyeColor = "";
-        }
-  
-  
-        $bindings = [
-          [':name',$post['name'],'str'],
-          [':phone',$post['phone'],'str'],
-          [':state',$post['state'],'str'],
-          [':financial',$financial,'str'],
-          [':eye',$eyeColor,'str']
-        ];
-  
-        $result = $pdo->otherBinded($sql, $bindings);
-  
-        if($result == "error"){
-          return getForm("<p>There was a problem processing your form</p>", $elementsArr);
-        }
-        else {
-          return getForm("<p>Contact Information Added</p>", $elementsArr);
-        }
-        
-}
+];
 
 function getForm($acknowledgement, $elementsArr){
     //global $stickyForm;
     
     /* THIS IS A HEREDOC STRING WHICH CREATES THE FORM AND ADD THE APPROPRIATE VALUES AND ERROR MESSAGES */
     $form = <<<HTML
-        <form method="post" action="index.php?page=addContact">
+        <form method="post" action="index.php?page=login">
         <div class="form-group">
-          <label for="name">Name (letters only){$elementsArr['name']['errorOutput']}</label>
-          <input type="text" class="form-control" id="name" name="name" value="{$elementsArr['name']['value']}" >
+          <label for="email">Email{$elementsArr['email']['errorOutput']}</label>
+          <input type="text" class="form-control" id="email" name="email" value="{$elementsArr['email']['value']}" >
         </div>
         <div class="form-group">
-          <label for="phone">Phone (format 999.999.9999) {$elementsArr['phone']['errorOutput']}</label>
-          <input type="text" class="form-control" id="phone" name="phone" value="{$elementsArr['phone']['value']}" >
+          <label for="password">Password{$elementsArr['password']['errorOutput']}</label>
+          <input type="text" class="form-control" id="password" name="password" value="{$elementsArr['password']['value']}" >
         </div>
-        
         <div>
-        <button type="submit" name="submit" class="btn btn-primary">Submit</button>
+        <button type="submit" name="submit" class="btn btn-primary">Login</button>
         </div>
       </form>
     HTML;
@@ -133,33 +83,3 @@ function getForm($acknowledgement, $elementsArr){
     
     }
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-  <meta charset="utf-8">
-  <meta http-equiv="x-ua-compatible" content="ie=edge">
-	<title>Login page</title>
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">	<link rel="stylesheet" href="../css/main.css">
-  </head>
-  <body>
-  <div class="container">
-    <div id="wrapper">
-      <header>
-        <br></br>
-        <h1>Login</h1>
-      </header>
-      <main>
-        <p class="error"><?php echo $error; ?></p>
-        <form action="login.php" method="post">
-          <div class="form-group">
-            <label>Email: <input type="text" name="email" class="form-control"></label>
-            <label>Password: <input type="password" name="password" class="form-control"></label>
-            <input type="submit" name="submit" value="Login" class="btn btn-primary">
-          </div>
-        </form> 
-      </main>
-    </div>
-  </div>
-  </body>
-</html>
