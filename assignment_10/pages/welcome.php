e30656b916a64bdf36dd686350bb034721685600
Hope <creatinghopeagain@gmail.com>
2023-04-28 15:26:08 -0400

    welcome page polish

--- assignment_10/pages/welcome.php
+++ assignment_10/pages/welcome.php
@@ -2,7 +2,8 @@
 
 function init(){
     $name = $_SESSION['name'];
-    return ["<h1>Welcome $name</h1>","<p>Welcome the stick form mod application. Click one of the lines above</p>"];
+    $status = $_SESSION['status'];
+    return ["<h1>Welcome $name</h1>","<p>Welcome to the $status homepage. Please click one of the lines above.</p>"];
 }
 
 ?>
